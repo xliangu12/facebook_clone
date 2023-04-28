7c9c70cd628d6d204c468a99772878d6f0593a76
ken_aikawa <xliangu@gmail.com>
2023-04-28 17:10:02 +0900

    pictureコントローラ設定

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,42 @@
+class PicturesController < ApplicationController
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+
+    if @picture.save
+      redirect_to @picture
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update(picture_params)
+      redirect_to @picture
+    else
+      render 'edit'
+    end
+  end 
+
+    private
+    def picture_params
+      params.require(:picture).permit(:title, :description, :image)
+    end
+end
