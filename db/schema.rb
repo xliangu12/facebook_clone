cc8fd1d5d5dcb127d9ac92f9f7d95d745d20aab5
ken_aikawa <xliangu@gmail.com>
2023-05-10 18:14:41 +0900

    before_validationというコールバック追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_10_012434) do
+ActiveRecord::Schema.define(version: 2023_05_10_090941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2023_05_10_012434) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
