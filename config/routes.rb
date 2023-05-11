ddb2d0d082c2439bfae46fdb23b0dc2e2e6e5c1c
ken_aikawa <xliangu@gmail.com>
2023-05-11 13:58:24 +0900

    routes rbにresources追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :feeds
+  resources :feeds do
+    collection do
+      post :confirm
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :pictures
+    end
 end
