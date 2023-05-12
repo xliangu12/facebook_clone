21cc28faf3f55d64e71eb7db5ab25b74302bb683
ken_aikawa <xliangu@gmail.com>
2023-05-12 11:16:12 +0900

    post model作成 feed rb 紐付け

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,5 @@
 class Feed < ApplicationRecord
+  validates :image, presence: true
   mount_uploader :image, ImageUploader
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :feeds
   resources :feeds do
     collection do
       post :confirm
+    end   
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :pictures
-    end
 end
