b7f8db002c169636a66bebe6ffc379755b30ad82
ken_aikawa <xliangu@gmail.com>
2023-05-15 14:37:40 +0900

    view users ファイル追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-  # before_action :login_required
-
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from Exception, with: :render_500
-  
-  def render_404
-    render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
-  end
-  
-  def render_500
-    render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
-  end
-
-
+  before_action :login_required
   private
 
-  # def login_required
-  #   redirect_to new_session_path unless current_user
-  # end
+  def login_required
+    redirect_to new_session_path unless current_user
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  # skip_before_action :login_required, only: [:new, :create]
+  skip_before_action :login_required, only: [:new, :create]
   def new
   end
 
